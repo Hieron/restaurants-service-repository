ac634f3e189df0a61852408fd1c913232d9503e1
Hieron <hieron.giacomini@gmail.com>
2024-04-08 23:10:04 -0300

    code: Add cors to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from app.database.db import db
 from flask import Flask
+from flask_cors import CORS
 from app.routes.restaurant_routes import restaurant_routes
 
 def create_app(Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, origins='*')
     db.init_app(app)
     app.register_blueprint(restaurant_routes, url_prefix='/api')
 
